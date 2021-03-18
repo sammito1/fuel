37a394f21c647288b0a249f9247da2f17545e62a
sammito1 <sam.c.melendez2@gmail.com>
2021-03-18 17:21:20 -0500

    modified forms.py

--- fuel_project/fuel_app/forms.py
+++ fuel_project/fuel_app/forms.py
@@ -2,13 +2,16 @@ from django import forms
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=20, label='',
-        widget=forms.TextInput(attrs={'class': 'form-group form-control', 'placeholder':'Username'}))
+        widget=forms.TextInput(attrs={'class': 'form-group form-control', 'placeholder':'Username'}),
+        error_messages={'required': "Enter a valid username"})
     password = forms.CharField(max_length=256, label='',
-        widget=forms.TextInput(attrs={'class': 'form-group form-control', 'placeholder':'Password'}))
+        widget=forms.TextInput(attrs={'class': 'form-group form-control', 'placeholder':'Password'}),
+        error_messages={'required': 'Enter a valid password'})
 
 class RegistrationForm(forms.Form):
     username = forms.CharField(max_length=20, label='',
-        widget=forms.TextInput(attrs={'class': 'form-group form-control', 'placeholder':'Username'}))
+        widget=forms.TextInput(attrs={'class': 'form-group form-control', 'placeholder':'Username'}),
+        error_messages={'required': 'Please enter a valid username'})
     password = forms.CharField(max_length=256, label='',
         widget=forms.TextInput(attrs={'class': 'form-group form-control', 'placeholder':'Password'}))
     confirm_password = forms.CharField(max_length=256, label='',
