da4175ef62ad896d6877bdf1f7e00a9415c30c8d
sammito1 <sam.c.melendez2@gmail.com>
2021-03-18 18:28:25 -0500

    Merging changes

--- fuel_project/fuel_app/forms.py
+++ fuel_project/fuel_app/forms.py
@@ -18,5 +18,9 @@ class RegistrationForm(forms.Form):
         widget=forms.TextInput(attrs={'class': 'form-group form-control', 'placeholder':'Confirm password'}))
 
 class QuoteForm(forms.Form):
-    gallons = forms.IntegerField(label="gallons", min_value=0, required=True, widget=forms.NumberInput(attrs={'class': 'form-control'}), error_messages={'required': "Please enter a valid number."})
-    delivery_date = forms.DateField(label="delivery-date", required=True, widget=forms.DateInput(attrs={'class': 'form-control'}), error_messages={'required': "Please select a date."})
+    gallons = forms.IntegerField(label='gallons', min_value=0, required=True,
+        widget=forms.NumberInput(attrs={'class': 'form-control'}),  
+        error_messages={'required': "Please enter a valid number."})
+    delivery_date = forms.DateField(label='delivery-date', required=True, 
+        widget=forms.DateInput(attrs={'class': 'form-control'}), 
+        error_messages={'required': "Please select a date."})
